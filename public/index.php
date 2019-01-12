2a2f19cbf6f53ff9e174992ff87f56bfc69ddc8e
nurgeld <nurgeldy@gmail.com>
2019-01-12 16:01:44 +0600

    add template for users

--- public/index.php
+++ public/index.php
@@ -10,8 +10,25 @@ $configuration = [
 
 $app = new \Slim\App($configuration);
 
+$container = $app->getContainer();
+$container['renderer'] = new \Slim\Views\PhpRenderer(__DIR__ . '/../templates');
+
+$app->get('/users/{id}', function ($request, $response, $args) {
+    $params = ['id' => $args['id']];
+
+    return $this->renderer->render($response, 'users/show.phtml', $params);
+});
+
 $app->get('/', function ($request, $response) {
     return $response->write('Welcome to Slim!');
+});
+
+$app->get('/users', function ($request, $response) {
+    return $response->write('GET /users');
+});
+
+$app->post('/users', function ($request, $response) {
+    return $response->write('POST /users');
 });
 
 $app->run();
